e1a7a70bd08a323c2f181321892d556fc6c1a1df
Viniccius Wirz <vinimovel23@gmail.com>
2024-03-11 20:41:52 -0300

    feat: backend llama chat session

--- gepeto-express/src/app.js
+++ gepeto-express/src/app.js
@@ -5,8 +5,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Health Check");
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const newDir = __dirname.substring(0, __dirname.length - 3);
+
+const model = new LlamaModel({
+  modelPath: path.join(newDir, "models", "greennodelm-7b-v4leo.Q4_K_M.gguf"),
+});
+const context = new LlamaContext({ model });
+const session = new LlamaChatSession({ context });
+
+app.post("/", async (req, res) => {
+  const question = req.body.question;
+  if (!question || question.length === 0) {
+    res.status().send("Não pode ser string vazia!");
+  }
+  const answer = await session.prompt(question);
+  const answeredAt = new Date();
+  res.send({ question, answer, answeredAt });
 });
 
 const port = 8080;
